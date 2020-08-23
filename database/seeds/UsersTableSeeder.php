a8986f1248f591a76fdb419a3b668f91edb54aa2
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-23 23:09:57 +0200

    Create the seeders for user and role tables

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'role_id' => '1',
+            'name' => 'Admin',
+            'username' => 'usamaeltmsah',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('adminPass'),
+        ]);
+
+        DB::table('roles')->insert([
+            'role_id' => '2',
+            'name' => 'Author',
+            'username' => 'alimohamed',
+            'email' => 'author@gmail.com',
+            'password' => bcrypt('authorPass'),
+        ]);
+    }
+}
